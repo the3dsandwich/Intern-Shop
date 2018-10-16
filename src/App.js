05cb77648965d3d27887f412f322fcde3bfa83f7
the3dsandwich <duhweiwei1013@gmail.com>
2018-10-16 22:16:58 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import InternInfo from "./InternshipCompanies.json";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={style.div}>
+        {InternInfo.map(listed => (
+          <div>
+            <p
+              style={style.item}
+              onClick={() => window.open(listed.link, "_blank")}
+            >
+              <h3 style={style.h3}>{listed.name}</h3>
+              <i>{listed.company}:</i> {listed.intro}
+            </p>
+          </div>
+        ))}
       </div>
     );
   }
 }
+
+const style = {
+  div: { fontFamily: "sans-serif", backgroundColor: "#777799", padding: "1em" },
+  item: {
+    backgroundColor: "#BBBBFF",
+    margin: "1em",
+    padding: "1em",
+    marginBottom: "1.5em"
+  },
+  h3: {
+    fontSize: "1.5em",
+    margin: 0,
+    padding: 0
+  }
+};
 
 export default App;
