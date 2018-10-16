15fadf8b055abd01fcced02ed9dcc975ea0b4122
the3dsandwich <duhweiwei1013@gmail.com>
2018-10-17 00:11:21 +0800

    key

---
+++ src/key/key.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyC2KvpT5d2rXJC5A6hNe1FOUCiUUog3XOo",
+  authDomain: "intern-shop.firebaseapp.com",
+  databaseURL: "https://intern-shop.firebaseio.com",
+  projectId: "intern-shop",
+  storageBucket: "intern-shop.appspot.com",
+  messagingSenderId: "1019203122045"
+};
+
+export default firebaseConfig;
